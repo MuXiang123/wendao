57aaf9f027aa937c6e2148509105f40941b0ebb4
muxiang123 <347213872@qq.com>
2023-03-01 15:30:54 +0800

    项目搭建

---
+++ src/main/java/com/example/wendao/WendaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wendao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WendaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WendaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wendao/WendaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wendao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WendaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
