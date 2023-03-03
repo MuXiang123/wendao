b32b8def33cf928372994e7bbabf9932ab84444a
muxiang123 <347213872@qq.com>
2023-03-03 17:06:07 +0800

    添加websocket功能

---
+++ src/main/java/com/example/wendao/websocket/ChatEndPoint.java
@@ -0,0 +1,105 @@
+package com.example.wendao.websocket;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.wendao.entity.Message;
+import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author: zhk
+ * @description:
+ * @date: 2023/3/3 16:49
+ * @version: 1.0
+ */
+@Component
+@ServerEndpoint(value = "/chat/{userId}")
+public class ChatEndPoint {
+    private Session session;
+
+    private static Map<String, ChatEndPoint> onlineUsers = new ConcurrentHashMap<>();
+
+    private static Logger logger = LoggerFactory.getLogger(ChatEndPoint.class);
+
+    private static int onlineCount = 0;
+
+    private String userId = "";
+
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("userId") String userId) {
+        this.session = session;
+        this.userId = userId;
+        // 这里是将其加入到线程安全的Map中
+        if (onlineUsers.containsKey(userId)) {
+            onlineUsers.remove(userId);
+            onlineUsers.put(userId, this);
+        } else {
+            onlineUsers.put(userId, this);
+            onlineUsers.put("18392710807", this);
+            logger.info(userId + "成功上线");
+            addOnlineCount();
+        }
+    }
+
+    @OnMessage
+    /**
+     * 用户之间一对一消息发送,这个message参数是从哪传递过来的？
+     */
+    public void onMessage(String message, Session session) {
+        try {
+            // 将message字符串进行反序列化
+            System.out.println("message------------" + message);
+            System.out.println(message);
+            Message messA = JSONObject.parseObject(message, Message.class);
+            // 这里相当于是上面请求路径上的userId
+            String toId = messA.getToId();
+            String messageContent = messA.getMessageContent();
+            String fromId = messA.getFromId();
+            logger.info(fromId + "向" + toId + "发送消息：" + messageContent);
+
+            // 这里应该重新构建一个新的Message对象
+            //Message messB = new Message();
+            System.out.println(fromId);
+            if (!Strings.isBlank(fromId)) {
+                onlineUsers.get(fromId).session.getBasicRemote().sendText(message);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClose
+    public void onClose() {
+        if (onlineUsers.containsKey(userId)) {
+            // 移除该客户端对象
+            onlineUsers.remove(userId);
+            // 在线用户数减1
+            subOnlineCount();
+        }
+    }
+
+    public static synchronized void addOnlineCount() {
+        ChatEndPoint.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        ChatEndPoint.onlineCount--;
+    }
+
+    public static synchronized int onlineCount() {
+        return onlineCount;
+    }
+
+}
